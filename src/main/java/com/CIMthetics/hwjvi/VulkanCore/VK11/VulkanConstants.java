c83970786bf703854bfecd9654bdd24a77a4366a
Douglas Kaip <dkaip@earthlink.net>
2019-03-18 17:16:44 -0600

    Added copyright information

--- src/main/java/com/CIMthetics/hwjvi/VulkanCore/VK11/VulkanConstants.java
+++ src/main/java/com/CIMthetics/hwjvi/VulkanCore/VK11/VulkanConstants.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Douglas Kaip
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.CIMthetics.hwjvi.VulkanCore.VK11;
 
 import com.CIMthetics.hwjvi.VulkanCore.VK11.Enums.VkStructureType;
@@ -6,7 +21,7 @@ import com.CIMthetics.hwjvi.VulkanExtensions.VK11.Enums.VkDebugReportObjectTypeE
 
 public class VulkanConstants
 {
-    public static final int     VK_HEADER_VERSION = 85;
+    public static final int     VK_HEADER_VERSION = 101;
     public static final float   VK_LOD_CLAMP_NONE               = 1000.0f;
     public static final int     VK_REMAINING_MIP_LEVELS         = -1;
     public static final int     VK_REMAINING_ARRAY_LAYERS       = -1;

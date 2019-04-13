34b37e06bf9c251bdf3f0280b46a2604873ea3b0
Douglas Kaip <dkaip@earthlink.net>
2019-04-12 18:09:52 -0600

    First unit test

---
+++ src/test/java/com/CIMthetics/jvulkan/VulkanCore/VK11/Enums/VkAccessFlagBitsTest.java
@@ -0,0 +1,16 @@
+package com.CIMthetics.jvulkan.VulkanCore.VK11.Enums;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VkAccessFlagBitsTest
+{
+
+    @Test
+    void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}

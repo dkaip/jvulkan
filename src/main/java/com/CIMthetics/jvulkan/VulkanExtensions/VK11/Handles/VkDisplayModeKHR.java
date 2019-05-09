7a5cbf8a8dfce68fc0379ba77943a8ba62c25ef1
Douglas Kaip <dkaip@earthlink.net>
2019-05-08 18:11:31 -0600

    Added function vkCreateDisplayModeKHR

---
+++ src/main/java/com/CIMthetics/jvulkan/VulkanExtensions/VK11/Handles/VkDisplayModeKHR.java
@@ -0,0 +1,8 @@
+package com.CIMthetics.jvulkan.VulkanExtensions.VK11.Handles;
+
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Handles.VulkanHandle;
+
+public class VkDisplayModeKHR extends VulkanHandle
+{
+
+}

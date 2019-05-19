bfc73bd97328f0d5d6f55993abb5002094eb03fd
Douglas Kaip <dkaip@earthlink.net>
2019-05-19 17:48:47 -0600

    Added stuff for pNext chain for VkSubmitInfo...since most was windows stuff it was not tested

---
+++ src/main/java/com/CIMthetics/jvulkan/VulkanExtensions/VK11/Structures/VkD3D12FenceSubmitInfoKHR.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Douglas Kaip
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.CIMthetics.jvulkan.VulkanExtensions.VK11.Structures;
+
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Enums.VkStructureType;
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Structures.CreateInfos.VulkanCreateInfoStructure;
+
+public class VkD3D12FenceSubmitInfoKHR extends VulkanCreateInfoStructure
+{
+    private long[]  waitSemaphoreValues;
+    private long[]  signalSemaphoreValues;
+
+    public VkD3D12FenceSubmitInfoKHR()
+    {
+        super(VkStructureType.VK_STRUCTURE_TYPE_D3D12_FENCE_SUBMIT_INFO_KHR);
+    }
+
+    public long[] getWaitSemaphoreValues()
+    {
+        return waitSemaphoreValues;
+    }
+
+    public void setWaitSemaphoreValues(long[] waitSemaphoreValues)
+    {
+        this.waitSemaphoreValues = waitSemaphoreValues;
+    }
+
+    public long[] getSignalSemaphoreValues()
+    {
+        return signalSemaphoreValues;
+    }
+
+    public void setSignalSemaphoreValues(long[] signalSemaphoreValues)
+    {
+        this.signalSemaphoreValues = signalSemaphoreValues;
+    }
+
+}

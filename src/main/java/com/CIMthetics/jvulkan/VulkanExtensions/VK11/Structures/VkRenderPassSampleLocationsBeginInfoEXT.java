dbea4c0168cc6ef5497e762659386f202a01a145
Douglas Kaip <dkaip@earthlink.net>
2019-05-21 14:30:57 -0600

    Add pNext chain stuff for VkRenderPassBeginInfo...not tested yet

---
+++ src/main/java/com/CIMthetics/jvulkan/VulkanExtensions/VK11/Structures/VkRenderPassSampleLocationsBeginInfoEXT.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 Douglas Kaip
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.CIMthetics.jvulkan.VulkanExtensions.VK11.Structures;
+
+import java.util.Collection;
+
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Enums.VkStructureType;
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Structures.CreateInfos.VulkanCreateInfoStructure;
+
+public class VkRenderPassSampleLocationsBeginInfoEXT extends VulkanCreateInfoStructure
+{
+    private Collection<VkAttachmentSampleLocationsEXT>  attachmentInitialSampleLocations;
+    private Collection<VkSubpassSampleLocationsEXT>     postSubpassSampleLocations;
+
+    public VkRenderPassSampleLocationsBeginInfoEXT()
+    {
+        super(VkStructureType.VK_STRUCTURE_TYPE_RENDER_PASS_SAMPLE_LOCATIONS_BEGIN_INFO_EXT);
+    }
+
+    public Collection<VkAttachmentSampleLocationsEXT> getAttachmentInitialSampleLocations()
+    {
+        return attachmentInitialSampleLocations;
+    }
+
+    public void setAttachmentInitialSampleLocations(
+            Collection<VkAttachmentSampleLocationsEXT> attachmentInitialSampleLocations)
+    {
+        this.attachmentInitialSampleLocations = attachmentInitialSampleLocations;
+    }
+
+    public Collection<VkSubpassSampleLocationsEXT> getPostSubpassSampleLocations()
+    {
+        return postSubpassSampleLocations;
+    }
+
+    public void setPostSubpassSampleLocations(
+            Collection<VkSubpassSampleLocationsEXT> postSubpassSampleLocations)
+    {
+        this.postSubpassSampleLocations = postSubpassSampleLocations;
+    }
+
+}

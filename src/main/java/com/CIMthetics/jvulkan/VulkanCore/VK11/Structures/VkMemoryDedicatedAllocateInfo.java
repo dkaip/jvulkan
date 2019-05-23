695979cb28d75fed8d93e23e77c5bfdff3a93b43
Douglas Kaip <dkaip@earthlink.net>
2019-05-23 17:52:54 -0600

    Add pNext chain stuff for VkMemoryAllocateInfo

---
+++ src/main/java/com/CIMthetics/jvulkan/VulkanCore/VK11/Structures/VkMemoryDedicatedAllocateInfo.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 Douglas Kaip
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.CIMthetics.jvulkan.VulkanCore.VK11.Structures;
+
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Enums.VkStructureType;
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Handles.VkBuffer;
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Handles.VkImage;
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Structures.CreateInfos.VulkanCreateInfoStructure;
+
+public class VkMemoryDedicatedAllocateInfo extends VulkanCreateInfoStructure
+{
+    private VkImage     image;
+    private VkBuffer    buffer;
+
+    public VkMemoryDedicatedAllocateInfo(VkStructureType sType)
+    {
+        super(VkStructureType.VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO);
+    }
+
+    public VkImage getImage()
+    {
+        return image;
+    }
+
+    public void setImage(VkImage image)
+    {
+        this.image = image;
+    }
+
+    public VkBuffer getBuffer()
+    {
+        return buffer;
+    }
+
+    public void setBuffer(VkBuffer buffer)
+    {
+        this.buffer = buffer;
+    }
+
+}

93a8fe226e55a664f69b1fec64d28284b4ed0e22
Douglas Kaip <dkaip@earthlink.net>
2019-05-28 15:08:35 -0600

    Work for adding pNext chain stuff for VkPipelineViewportStateCreateInfo

---
+++ src/main/java/com/CIMthetics/jvulkan/VulkanExtensions/VK11/Structures/CreateInfos/VkPipelineViewportWScalingStateCreateInfoNV.java
@@ -0,0 +1,9 @@
+package com.CIMthetics.jvulkan.VulkanExtensions.VK11.Structures.CreateInfos;
+
+import com.CIMthetics.jvulkan.VulkanCore.VK11.Structures.CreateInfos.VulkanCreateInfoStructure;
+
+public class VkPipelineViewportWScalingStateCreateInfoNV
+        extends VulkanCreateInfoStructure
+{
+
+}
